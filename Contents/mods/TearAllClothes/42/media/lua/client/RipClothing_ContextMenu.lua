8ae5450e50e1ef97afbab7201059fe46bf74fbb2
Steven Turturo <steven@turturo.dev>
2025-07-19 08:20:11 -0400

    feat: added some more checks and a 3rd option to the menu for selected,surrounding, and all

--- Contents/mods/TearAllClothes/42/media/lua/client/RipClothing_ContextMenu.lua
+++ Contents/mods/TearAllClothes/42/media/lua/client/RipClothing_ContextMenu.lua
@@ -1,7 +1,16 @@
 require "ISUI/ISInventoryPaneContextMenu"
 
+-- check to see
+local function skipInventoryItem(playerObj, container, skipInventory)
+    local playerInv = playerObj:getInventory()
+    if (skipInventory) and (container == playerInv) then 
+        print("Skipping inventory item")  -- for debugging
+        return true
+    end
+    return false
+end
 
-local function getSurroundingClothingItems(playerObj)
+local function getAllClothingItems(playerObj, skipInventory)
     local clothingItems = {}
     local containers = ISInventoryPaneContextMenu.getContainers(playerObj)
     for i = 0, containers:size()-1 do
@@ -9,8 +18,10 @@ local function getSurroundingClothingItems(playerObj)
         local containerItems = container:getItems()
         for j = 0, containerItems:size()-1 do
             local item = containerItems:get(j)
-            if (instanceof(item, "Clothing") and item:getFabricType()) and not (playerObj:isEquipped(item) or item:isFavorite()) then
-                table.insert(clothingItems, item)
+            if (instanceof(item, "Clothing") and item:getFabricType()) and
+                not (playerObj:isEquipped(item) or item:isFavorite()) and
+                not skipInventoryItem(playerObj, container, skipInventory) then
+                    table.insert(clothingItems, item)
             end
         end
     end
@@ -37,7 +48,7 @@ local function playerHasToolRequired(player, clothingItem)
     end
 end
 
-local function ripSelectedClothing(player, clothingItems)
+local function tearSelectedClothing(player, clothingItems)
     for _, clothingItem in ipairs(clothingItems) do
         local hasRequired = playerHasToolRequired(player, clothingItem)
         if (not (player:isEquipped(clothingItem) or clothingItem:isFavorite()) and hasRequired) then
@@ -48,8 +59,9 @@ local function ripSelectedClothing(player, clothingItems)
 end
 
 
-local function ripNearbyClothing(player)
-    local clothingItems = getSurroundingClothingItems(player)
+local function tearClothing(player, skipInventoryItems)
+    print("Skip inventory items: ", skipInventoryItems)
+    local clothingItems = getAllClothingItems(player, skipInventoryItems)
     for _, clothingItem in ipairs(clothingItems) do
         local hasRequired = playerHasToolRequired(player, clothingItem)
         if (not (player:isEquipped(clothingItem) or clothingItem:isFavorite()) and hasRequired) then
@@ -66,9 +78,17 @@ local function createRipClothingMenu(player, context, items)
     local clothingItemsTotal = 0
     -- the items are either a InventoryItem[] or ContextMenuItemStack[]
     for _, ctxitem in ipairs(items) do
-        if ctxitem.items[1]:getCategory() == "Clothing" and not ctxitem.equipped then
-            table.insert(selectedClothingItems, ctxitem.items[1])
-            clothingItemsTotal = clothingItemsTotal + 1
+        if ctxitem["items"] then
+            if ctxitem.items[1]:getCategory() == "Clothing" and not ctxitem.equipped then
+                table.insert(selectedClothingItems, ctxitem.items[1])
+                clothingItemsTotal = clothingItemsTotal + 1
+            end
+        else 
+            if ctxitem:getCategory() == "Clothing" and not playerObj:isEquipped(ctxitem) then
+                print("Inventory Item was expanded")
+                table.insert(selectedClothingItems, ctxitem)
+                clothingItemsTotal = clothingItemsTotal + 1
+            end
         end
     end
 
@@ -79,9 +99,13 @@ local function createRipClothingMenu(player, context, items)
 
         local subMenu = context:getNew(context)
         context:addSubMenu(option, subMenu)
-        local selectedOption = subMenu:addOption("Tear Selected Clothing", playerObj, ripSelectedClothing,
+        local selectedOption = subMenu:addOption(getText("ContextMenu_TearClothingSelected"), playerObj, tearSelectedClothing,
             selectedClothingItems)
-        local allOption = subMenu:addOption("Tear All Nearby Clothing", playerObj, ripNearbyClothing)
+        selectedOption.iconTexture = getTexture("media/textures/target.png")
+        local surroundingOption = subMenu:addOption(getText("ContextMenu_TearClothingSurrounding"), playerObj, tearClothing, true)
+        surroundingOption.iconTexture = getTexture("media/textures/radar.png")
+        local allOption = subMenu:addOption(getText("ContextMenu_TearClothingAll"), playerObj, tearClothing, false)
+        allOption.iconTexture = getTexture("media/textures/globe.png")
     end
 end
 
